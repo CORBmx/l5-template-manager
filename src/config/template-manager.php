73bf0e5656f993eb30db3a58e6f4f663795c7285
Gabriel Ortiz <gabriel.ortiz@corb.mx>
2016-03-22 20:06:20 -0600

    Added new configuration options

--- src/config/template-manager.php
+++ src/config/template-manager.php
@@ -2,11 +2,18 @@
 
     return [
 
+        /**
+         * Array of models
+         * Use this to obtain database possible fields, required!.
+         * Ex: App\User::class
+         */
         'models' => [
-            
+
         ],
+        'use_routes'      => true,
+        'route'           => 'templates',
+        'template_table'  => 'templates'  //Dont change this option if migration is completed, otherwise rollback will not work.
 
 
     ];
 
-?>
